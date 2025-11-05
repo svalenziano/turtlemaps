3d5666243aa54762ae1effd2e49f23d7a95ffbb7
SValenziano <senorvalenz@gmail.com>
2025-11-05 17:14:44 -0500

    SVG map WIP

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,37 @@
+// TYPES
+type bboxArray = [number, number, number, number];
 
 
 function makeSVGElement(type: "svg" | "rect" | "circle" | "polygon" | "path" | "g"): SVGElement {
   return document.createElementNS("http://www.w3.org/2000/svg", type);
 }
 
+class MapApp {
+  data: object;  // TODO - import OSM types
+  bbox: bboxArray | null;
+  query: string | null;
+  centroid: [number, number] | null;
+
+  constructor() {
+    this.data = {};
+    this.bbox = null;
+    this.query = null;
+    this.centroid = null;
+  }
+
+  async init() {
+    const response = await fetch("./data/durham_nc.json");
+    const json = await response.json();
+    this.bbox = json.bbox;
+    this.centroid = json.centroid;
+
+    const elements: OSMElement[] = json.elements
+  }
+}
+
+class svgMap {
+
+}
 
 document.addEventListener("DOMContentLoaded", () => {
   const WIDTH = "300";
